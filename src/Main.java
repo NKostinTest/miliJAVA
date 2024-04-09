5b46460b554ea64919c7a334e8de1e96ca3c65ee
Nikolai <kolyakos2016@yandex.ru>
2024-04-09 11:17:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int стоимостьБилета = 10000;
+        int бонусныеМили = 20;
+
+        int количествоМилей = стоимостьБилета / бонусныеМили;
+        System.out.println(количествоМилей);
+
+    }
+}
